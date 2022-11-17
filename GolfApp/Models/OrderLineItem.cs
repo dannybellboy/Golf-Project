d92798a2f158091b9f5a802d5fb837ae14997318
dannybellboy <90338634+dannybellboy@users.noreply.github.com>
2022-11-17 13:14:49 -0700

    Models: CustomProduct, ModelFlex, Order1, OrderLineItem, ShaftBrandModelFlex

---
+++ GolfApp/Models/OrderLineItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GolfApp.Models
+{
+    public class OrderLineItem
+    {
+        [Key]
+        [Required]
+        public int orderID { get; set; }
+        public int quantity { get; set; }
+        public DateTime date { get; set; }
+        // foreign key relationship
+        public int customID { get; set; }
+        public CustomProduct customproduct { get; set; }
+    }
+}
---
+++ GolfApp/Models/ShaftBrandModelFlex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GolfApp.Models
+{
+    public class ShaftBrandModelFlex
+    {
+      [Key]
+      [Required]
+      public int sbmfID { get; set; }
+      //foreign key relationships
+      public int shaftID { get; set; }
+      public Shaft shaft { get; set; }
+      public int modelFlexID { get; set; }
+      public ModelFlex modelflex { get; set; }
+      public int brandID { get; set; }
+      public Brand brand { get; set; }
+        
+    }
+}
