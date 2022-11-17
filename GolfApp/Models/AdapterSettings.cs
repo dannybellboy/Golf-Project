a683ef08e66c476ec3e3cc4e4c085916c5a95663
David Shipley <dave.r.shipley@gmail.com>
2022-11-17 12:33:01 -0700

    Brand, GripModel, and AdapterSettings Models

---
+++ GolfApp/Models/AdapterSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GolfApp.Models
+{
+    public class AdapterSettings
+    {
+        [Key]
+        [Required]
+        public int adapterID { get; set; }
+        public string adapterName { get; set; }
+        public string adapterSettings { get; set; }
+        //Build Foreign Key with brand
+        public int brandID { get; set; }
+        public Brand brand { get; set; }
+    }
+}
+
---
+++ GolfApp/Models/GripModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GolfApp.Models
+{
+    public class GripModel
+    {
+        [Key]
+        [Required]
+        public int gripID { get; set; }
+        //Build Foreign Key with brand
+        public int brandID { get; set; }
+        public Brand brand { get; set; }
+        
+    }
+}
+
