1175b598b7defd4f4cb43720dfbe4fd7970fd3da
ehorlacher <90493176+ehorlacher@users.noreply.github.com>
2022-11-18 14:27:49 -0700

    build type, shaft, and shaft build type models

---
+++ GolfApp/Models/Shaft.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GolfApp.Models
+{
+    public class Shaft
+    {
+        [Key]
+        [Required]
+        public int shaftID { get; set; }
+        public string shaftName { get; set; }
+        public string length { get; set; }
+    }
+}
+
