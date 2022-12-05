c755a550f1095ac923549f574bdb21768176d502
dannybellboy <90338634+dannybellboy@users.noreply.github.com>
2022-12-05 13:59:40 -0700

    some product page updates

---
+++ GolfApp/Components/BuildTypesViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using GolfApp.Models;
+
+
+namespace GolfApp.Components
+{
+    public class BuildTypesViewComponent : ViewComponent
+    {
+        private BuildType repo { get; set; }
+
+        public BuildTypesViewComponent(BuildType temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var buildtypes = repo.typeName
+                .Distinct()
+                .ToList();
+
+
+            return View(buildtypes);
+        }
+    }
+}
